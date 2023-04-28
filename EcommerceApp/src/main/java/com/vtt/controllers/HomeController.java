a4f7741b9e910ee03d37833a80903fd69d8aa4eb
vutrongthang <100742325+vutrongthang@users.noreply.github.com>
2023-04-29 00:18:32 +0700

    update

--- EcommerceApp/src/main/java/com/vtt/controllers/HomeController.java
+++ EcommerceApp/src/main/java/com/vtt/controllers/HomeController.java
@@ -21,6 +21,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -38,38 +39,21 @@ public class HomeController {
     private CategoryService categoryService;
     @Autowired
     private ProductService productService;
-//    @RequestMapping("/")
-//    public String index(Model model,
-//            @RequestParam(value = "kw", defaultValue = "") String kw) {
-//        List<Category> cates = new ArrayList<>();
-//        cates.add(new Category(1, "Moblie"));
-//        cates.add(new Category(2, "Tablet"));
-//        cates.add(new Category(3, "Desktop"));
-//
-//        List<Products> products = new ArrayList<>();
-//        products.add(new Products(1, "Iphone 14 Promax", "https://cdn.tgdd.vn/hoi-dap/1421873/top-6-dien-thoai-man-hinh-full-hd-gia-re-tai-the-gioi-di-1-800x450.jpg", new BigDecimal(34000000)));
-//        products.add(new Products(2, "Samsung 14 Promax", "https://cdn.tgdd.vn/hoi-dap/1421873/top-6-dien-thoai-man-hinh-full-hd-gia-re-tai-the-gioi-di-1-800x450.jpg", new BigDecimal(34000000)));
-//        products.add(new Products(3, "Iphone 14 Promax", "https://cdn.tgdd.vn/hoi-dap/1421873/top-6-dien-thoai-man-hinh-full-hd-gia-re-tai-the-gioi-di-1-800x450.jpg", new BigDecimal(34000000)));
-//        products.add(new Products(4, "Iphone 14 Promax", "https://cdn.tgdd.vn/hoi-dap/1421873/top-6-dien-thoai-man-hinh-full-hd-gia-re-tai-the-gioi-di-1-800x450.jpg", new BigDecimal(34000000)));
-//        products.add(new Products(5, "Iphone 14 Promax", "https://cdn.tgdd.vn/hoi-dap/1421873/top-6-dien-thoai-man-hinh-full-hd-gia-re-tai-the-gioi-di-1-800x450.jpg", new BigDecimal(34000000)));
-//
-//        if (kw != null && !kw.isEmpty()) {
-//            products = products.stream().filter(p -> p.getProductName().contains(kw)).collect(Collectors.toList());
-//        }
-//
-//        model.addAttribute("categories", cates);
-//        model.addAttribute("products", products);
-//        return "index";
-//    }
+
+    @ModelAttribute
+    public void commonAttributes(Model model) {
+        List<Category> cates = this.categoryService.getCategories();
+        model.addAttribute("categories", cates);
+
+    }
 
     @RequestMapping("/")
     public String index(Model model, @RequestParam Map<String, String> params) {
         List<Products> products = this.productService.getProducts(params);
-
-        model.addAttribute("categories", this.categoryService.getCategories());
         model.addAttribute("products", products);
         return "index";
     }
+
     @GetMapping("/products/{productId}")
     public String productDetail(Model model, @PathVariable(value = "productId") int id) {
         model.addAttribute("products", this.productService.getProductById(id));
