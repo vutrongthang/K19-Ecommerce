310670e18c845275c612c03e78beed26853d9267
vutrongthang <100742325+vutrongthang@users.noreply.github.com>
2023-05-03 23:22:24 +0700

    update

---
+++ EcommerceApp/src/main/java/com/vtt/configs/SpringSecurityConfig.java
@@ -0,0 +1,75 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.vtt.configs;
+
+import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+/**
+ *
+ * @author vutrongthang
+ */
+public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Autowired
+//    private LoginSuccessHandler loginHandler;
+//    @Autowired
+//    private MyLogoutSuccessHandler logoutHandler;
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth)
+            throws Exception {
+        auth.userDetailsService(userDetailsService)
+                .passwordEncoder(passwordEncoder());
+    }
+
+    @Override
+    protected void configure(HttpSecurity http)
+            throws Exception {
+        http.formLogin().loginPage("/login")
+                .usernameParameter("username")
+                .passwordParameter("password");
+
+        http.formLogin().defaultSuccessUrl("/")
+                .failureUrl("/login?error");
+//        http.formLogin().successHandler(this.loginHandler).failureUrl("/login?error");
+
+        http.logout().logoutSuccessUrl("/login");
+//        http.logout().logoutSuccessHandler(this.logoutHandler);
+        http.exceptionHandling()
+                .accessDeniedPage("/login?accessDenied");
+
+        http.authorizeRequests().antMatchers("/").permitAll()
+                .antMatchers("/admin/**")
+                .access("hasRole('ROLE_ADMIN')");
+        http.csrf().disable();
+    }
+
+    @Bean
+    public Cloudinary cloudinary() {
+        Cloudinary cloudinary
+                = new Cloudinary(ObjectUtils.asMap(
+                        "cloud_name", "dxxwcby8l",
+                        "api_key", "448651448423589",
+                        "api_secret", "ftGud0r1TTqp0CGp5tjwNmkAm-A",
+                        "secure", true));
+        return cloudinary;
+    }
+}
